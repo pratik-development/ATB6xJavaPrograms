1f66e68169c65d042bcf519984f0afdcec09e00b
pratik-development <pratik.development@gmail.com>
2024-06-22 23:19:18 +0530

    Learn about string and ternary operator

---
+++ src/ex_20042024/Lab052.java
@@ -0,0 +1,28 @@
+package ex_20042024;
+
+public class Lab052
+{
+    public static void main(String[] args)
+    {
+        String name = "Pramod";
+        // String?
+        // Bunch of Chars - Collection of Chars
+        // Class -> What is Class??
+        // Class -> provides some extra f(n).
+        System.out.println("Your name is -> "+ name);
+
+        /*
+        String S1 = "Pratik";
+        System.out.println(S1.toLowerCase()); // here toLowercase is available method
+
+        dark method are available method
+
+         */
+
+        /*
+                hover over String and click on right click and go to declaration or usages so it will
+                open declaration page
+         */
+
+    }
+}
---
+++ src/ex_20042024/Lab054.java
@@ -0,0 +1,12 @@
+package ex_20042024;
+
+public class Lab054
+{
+    String name = "Pratik";
+    // How they are different?
+    // String - immutable in nature?
+    // Strings are immutable , non-primitive data types in Java.
+    // Immutable - that can't be change.
+    //name = "Navale";
+
+}
